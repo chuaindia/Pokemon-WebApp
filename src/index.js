3ad703dee949c46a423984f1533fbc739bff79c0
Samuellekpor <samuellekpor@gmail.com>
2022-10-25 22:45:30 +0000

    Fetch data from pokeapi

--- src/index.js
+++ src/index.js
@@ -3,4 +3,26 @@ import './style.css';
 import pokelogo from './images/pokelogo.png';
 
 const logo = document.querySelector('.logo');
-logo.innerHTML = `<img src="${pokelogo}" alt="pokemon-logo">`;
+logo.innerHTML = `<img src="${pokelogo}" alt="pokemon-logo"> `;
+
+const mainUrl = 'https://pokeapi.co/api/v2/';
+
+const getPokemons = async () => {
+  const resp = await fetch(`${mainUrl}pokemon?limit=30`);
+  const pokemons = await resp.json();
+  pokemons.results.forEach(pokemon => {
+    fetchPokemon(pokemon);
+  });
+  
+}
+
+const fetchPokemon = async (pokemon) => {
+  let pokeurl = pokemon.url;
+  const response = await fetch(pokeurl);
+  const pokeData = await response.json();
+  return pokeData;
+}
+
+const displayPokemon = (pokemon) => {
+  
+}
