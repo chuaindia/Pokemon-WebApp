3765f3f0f56be6ea4b432fb9a2701f3e0725f498
Samuellekpor <samuellekpor@gmail.com>
2022-10-25 15:41:11 +0000

    Add logo image and correct header

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
 import './style.css';
+import pokelogo from './pokelogo.png';
 
+const logo = document.querySelector('.logo');
+logo.innerHTML = `<img src="${pokelogo}" alt="pokemon-logo">`
--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,17 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192
+            }
+          }
+        ]
+      }
     ],
   },
 };
