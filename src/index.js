503c21340455cd77d144a122a5571181960635a9
Samuellekpor <samuellekpor@gmail.com>
2022-10-27 08:39:23 +0000

    Add function to get likes

--- src/index.js
+++ src/index.js
@@ -4,8 +4,16 @@ import './style.css';
 import pokelogo from './images/pokelogo.png';
 import likeIcon from './images/like.png';
 
+const logo = document.querySelector('.logo');
+logo.innerHTML = `<img src="${pokelogo}" alt="pokemon-logo"> `;
+
 const mainUrl = 'https://pokeapi.co/api/v2/';
 const main = document.querySelector('main');
+
+const invoUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/';
+const appId = 'scLRTVqWFoqoog0Hjt1E';
+
+
 
 const displayPokemon = (pokemon) => {
   const pokeContainer = document.createElement('div');
@@ -15,7 +23,7 @@ const displayPokemon = (pokemon) => {
     <h2 class="pokeName">${pokemon.name}</h2>
     <div class="like">
       <img src="${likeIcon}" alt="likeIcon" class="likeIcon">
-      <span class="likeCount">9</span>
+      <span class="likeCount"></span>
     </div>
   </div>
   <button class="comment btn">Comments</button>
@@ -40,3 +48,26 @@ const getPokemons = async () => {
 };
 
 getPokemons();
+
+const getLikes = async () => {
+  const response = await fetch(`${invoUrl}${appId}/likes/`);
+  const data = await response.json();
+  return data;
+}
+
+const likeFields = document.querySelectorAll(".likeCount");
+console.log(likeFields);
+const displayLikes = (likes) => {
+  
+  likeFields.forEach((field) => {
+    let name = field.parentElement.previousElementSibling.innerHTML;
+    console.log(name);
+    let likeArr = likes.filter((el)=>{el["item_id"] == name}) || [];
+    let numberOfLikes = likeArr[0]["likes"] || 0;
+    field.innerHTML = `${numberOfLikes}`;
+  })
+}
+
+const likes = await getLikes();
+console.log(likes)
+displayLikes(likes);
