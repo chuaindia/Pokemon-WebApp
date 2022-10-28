ac7828e4f155ad0c0bb631b1caa7874ad709364c
Samuellekpor <samuellekpor@gmail.com>
2022-10-28 02:06:34 +0000

    Add function to get likes

--- src/index.js
+++ src/index.js
@@ -13,14 +13,20 @@ const main = document.querySelector('main');
 const invoUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/';
 const appId = 'scLRTVqWFoqoog0Hjt1E';
 
+const getLikes = async () => {
+  const response = await fetch(`${invoUrl}${appId}/likes/`);
+  const data = await response.json();
+  return data;
+}
 
+let likes = await getLikes();
 
 const displayPokemon = (pokemon) => {
   const pokeContainer = document.createElement('div');
   pokeContainer.className = 'pokeContainer';
   pokeContainer.innerHTML = `<img src="${pokemon.sprites.front_shiny}" alt="pokeImage" class="pokeImage">
   <div class="pokeAssets">
-    <h2 class="pokeName">${pokemon.name}</h2>
+    <h2 id="pokemon${pokemon.id}"class="pokeName">${pokemon.name}</h2>
     <div class="like">
       <img src="${likeIcon}" alt="likeIcon" class="likeIcon">
       <span class="likeCount"></span>
@@ -48,26 +54,3 @@ const getPokemons = async () => {
 };
 
 getPokemons();
-
-const getLikes = async () => {
-  const response = await fetch(`${invoUrl}${appId}/likes/`);
-  const data = await response.json();
-  return data;
-}
-
-const likeFields = document.querySelectorAll(".likeCount");
-console.log(likeFields);
-const displayLikes = (likes) => {
-  
-  likeFields.forEach((field) => {
-    let name = field.parentElement.previousElementSibling.innerHTML;
-    console.log(name);
-    let likeArr = likes.filter((el)=>{el["item_id"] == name}) || [];
-    let numberOfLikes = likeArr[0]["likes"] || 0;
-    field.innerHTML = `${numberOfLikes}`;
-  })
-}
-
-const likes = await getLikes();
-console.log(likes)
-displayLikes(likes);
